6af66649cf2f42e9879fa6e94aad672662ce153f
HamzaMohamoud <hamzamohamoud2324@gmail.com>
2023-05-08 23:42:39 -0700

    begin working on js file

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
   /*----- constants -----*/
+  const maxWrong = 6;
+  
+  const IMGS = [];
+
+
 
 
   /*----- state variables -----*/
-
+  let answer = " ";
+  let mistakes = 0; 
+  let wrongGuesses = [];
+  let allGuesses = [];
+  let wordStatus = null;
+  let gameStatus;
 
   /*----- cached elements  -----*/
+
+  
 
 
   /*----- event listeners -----*/
 
 
   /*----- functions -----*/
+
+  function init() {
+    allGuesses = [];
+    worngGuesses = [];
+    gameStatus = null;
+    wordStatus = [];
+    render();
+  };
+  init() 
